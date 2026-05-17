9c7a93daf518ec51d0754c46e2bca7b3bb4e050d
Radhesh-Roy <radheshroy0011@gmail.com>
2026-05-17 18:24:59 +0600

    add

---
+++ lib/module12/Custom_file/custom_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget{
+
+  final String hintText;
+  final bool obsecure;
+  final TextEditingController? controller;
+  final Icon? suffixIcon;
+  final Icon? prefixIcon;
+  final TextInputType? keyBoardType;
+
+  CustomFormField({
+    super.key, required this.hintText, this.obsecure, this.controller, this. suffixIcon, this.prefixIcon, this.keyBoardType
+  });
+  TextEditingController phoneC= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller:controller,
+      obscureText: obsecure,
+      keyboardType: keyBoardType,
+      decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          prefixIconColor: Colors.grey,
+          suffixIconColor: Colors.grey,
+          hintStyle: TextStyle(color: Colors.grey),
+          suffixIcon: suffixIcon,
+          border: OutlineInputBorder(
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: Colors.grey
+              )
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: Colors.green
+              )
+          ),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: Colors.red
+              )
+          ),
+          hintText: hintText
+      ),
+    );
+  }
+}
--- lib/module12/login_view/login.dart
+++ lib/module12/login_view/login.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../Custom_file/custom_form_field.dart';
 import '../Custom_file/custom_text.dart';
 
 class loginView extends StatefulWidget {
-  const loginView({super.key});
+   const loginView({super.key});
+
+
 
   @override
   State<loginView> createState() => _loginViewState();
+
+
 }
 
 class _loginViewState extends State<loginView> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +27,7 @@ class _loginViewState extends State<loginView> {
          children: [
            CustomText(text: "Wellcome Back ", fontSize: 22, fontWeight: FontWeight.w700,),
            CustomText(text: "Please Enter you credential"),
-           CustomFormField()
+           CustomFormField( hintText: "",)
          ],
        ),
      ),
@@ -30,13 +36,3 @@ class _loginViewState extends State<loginView> {
   }
 }
 
-class CustomFormField extends StatelessWidget {
-  const CustomFormField({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField();
-  }
-}
